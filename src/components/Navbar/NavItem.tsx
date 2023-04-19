ac2e92b99b6b5e56784e4012a10b902c04b2b8d7
Samuel Olakada <olakadasamuel@gmail.com>
2023-04-19 08:21:46 +0100

    chore: changed the width of navItems to fit

--- src/components/Navbar/NavItem.tsx
+++ src/components/Navbar/NavItem.tsx
@@ -4,7 +4,7 @@ import { NavItemProps } from "../../interfaces";
 function NavItem({ href, title, Icon }: NavItemProps) {
   return (
     <Link
-      className="hover:bg-color-grey-light transition-all delay-200 ease-in py-4 px-8 rounded-full"
+      className="hover:bg-color-grey-light w-fit transition-all delay-200 ease-in py-4 px-8 rounded-full"
       to={href}
     >
       <div className="flex text-3xl font-medium items-center gap-6">
