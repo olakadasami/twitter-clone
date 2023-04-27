d5ccb45b816eaf4b7c45dff8e7ce3b1c29b2a936
Samuel Olakada <olakadasamuel@gmail.com>
2023-04-27 17:28:07 +0100

    chore: created fake tweets array data

---
+++ src/components/Home/tweetList.ts
@@ -0,0 +1,33 @@
+import { TweetProps } from "../../interfaces";
+import reactImage from "../../assets/react.svg";
+
+export const tweetList: TweetProps[] = [
+  {
+    userImageUrl: reactImage,
+    name: "SAM-MEH",
+    username: "olakxyxz",
+    tweetMessage:
+      "ALorem ipsum dolor sit amet consectetur adipisicing elit. Est ratione, quo ad iure corrupti, praesentium inventore repellat delectus unde vitae molestias animi nihil placeat fugit. .        ",
+  },
+  {
+    userImageUrl: reactImage,
+    name: "SAM-MEH",
+    username: "olakxyxz",
+    tweetMessage:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ratione, quo ad iure corrupti, praesentium inventore repellat delectus unde vitae molestias animi nihil placeat fugit.",
+  },
+  {
+    userImageUrl: reactImage,
+    name: "SAM-MEH",
+    username: "olakxyxz",
+    tweetMessage:
+      "Lorem ipsum dolor. Molestiae dolores quam odio veritatis. Est ratione, quo ad iure",
+  },
+  {
+    userImageUrl: reactImage,
+    name: "SAM-MEH",
+    username: "olakxyxz",
+    tweetMessage:
+      "ZLorem ipsum dolor sit amet consectetur adipisicing elit. Est ratione, quo ad iure",
+  },
+];
