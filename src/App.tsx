c2d5e6d9ea79462fb4f9eae4bd675b5e59708107
Samuel Olakada <olakadasamuel@gmail.com>
2023-04-27 18:07:01 +0100

    chore: added routing in main.tsx, configured App.tsx for routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,23 @@
+import { Outlet } from "react-router-dom";
 import Navbar from "./components/Navbar/Navbar";
+import { useModalStore } from "./store/modalStore";
+import Modal from "./components/UI/Modal/Modal";
 
 function App() {
+  const isOpen = useModalStore((state) => state.open);
   return (
     <div className="flex">
       <section className="w-1/4 h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-color-grey-light scrollbar-track-white">
         <Navbar />
       </section>
-      <section className="w-2/4 shadow-md">Middle</section>
-      <section className="w-1/4">Right</section>
+      <section className="w-2/4 shadow-md h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-color-grey-light scrollbar-track-white">
+        <Outlet />
+      </section>
+      <section className="w-1/4 h-screen overflow-y-auto scrollbar-thin scrollbar-thumb-color-grey-light scrollbar-track-white">
+        Right
+      </section>
+
+      {isOpen && <Modal />}
     </div>
   );
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,47 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+// Static files
+import "./index.css";
+// Routes
+import Home from "./routes/Home";
+import Messages from "./routes/Messages";
+import Notifications from "./routes/Notifications";
+import Explore from "./routes/Explore";
+import Profile from "./routes/Profile";
+import Bookmarks from "./routes/Bookmarks";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "explore",
+        element: <Explore />,
+      },
+      {
+        path: "messages",
+        element: <Messages />,
+      },
+      {
+        path: "notifications",
+        element: <Notifications />,
+      },
+      {
+        path: "profile",
+        element: <Profile />,
+      },
+      {
+        path: "bookmarks",
+        element: <Bookmarks />,
+      },
+    ],
   },
 ]);
 
